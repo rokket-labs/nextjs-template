43d50bf7e0a7b7e32c53fa3147d9cb89c2b668ad
czhoffmann23 <camilozepedahoffmann@gmail.com>
2022-12-20 17:49:34 -0300

    :art: (design): designer comments

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { AnimateSharedLayout } from 'framer-motion'
 
 import { NavigationButtons } from './NavigationButtons'
@@ -7,19 +7,31 @@ import { NavigationLogo } from './NavigationLogo'
 export const Layout: React.FC = ({ children }) => {
   return (
     <AnimateSharedLayout>
-      <Flex
-        flexDir="column"
-        height="100vh"
-        width="100%"
-        backgroundImage={`url('background.png')`}
-      >
-        <Flex flex="2" p="0 4rem" justifyContent="space-between">
-          <NavigationLogo />
-          <NavigationButtons />
-        </Flex>
-        <Flex flex="22">
-          <Flex p="4rem" minW="100vw" minH="100%" overflow="auto">
-            {children}
+      <Flex position="relative" w="100%" h="100%">
+        <Box
+          position="absolute"
+          bg="red"
+          zIndex="1"
+          height="100vh"
+          width="100%"
+          backgroundImage={`url('background.png')`}
+        />
+        <Flex
+          position="absolute"
+          zIndex="2"
+          flexDir="column"
+          height="100vh"
+          width="100%"
+          bg="rgb(0,0,0,0.4)"
+        >
+          <Flex flex="2" p="0 4rem" justifyContent="space-between">
+            <NavigationLogo />
+            <NavigationButtons />
+          </Flex>
+          <Flex flex="22">
+            <Flex p="4rem" minW="100vw" minH="100%" overflow="auto">
+              {children}
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
