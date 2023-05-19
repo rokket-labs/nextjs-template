10a68a39ce1908fce2acb6c2015d9990cff25e6d
czhoffmann23 <camilozepedahoffmann@gmail.com>
2023-05-19 16:31:46 -0400

    :wrench: (next.config.js): removed unused lines

--- next.config.js
+++ next.config.js
@@ -2,14 +2,4 @@ module.exports = {
   images: {
     domains: ['i.imgur.com', 'www.google.com', 'imgur.com'],
   },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.(graphql|gql)$/,
-  //     exclude: /node_modules/,
-  //     loader: 'graphql-tag/loader',
-  //   })
-
-  //   return config
-  // },
-  // webpackDevMiddleware: config => config,
 }
